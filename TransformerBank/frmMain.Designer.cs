6f46cee69fb34f0c95cc50fc890bf3281a2e5c0a
Jakub Berthoty <berthoty@kros.sk>
2019-04-11 15:22:06 +0200

    Basic form settings.

--- TransformerBank/frmMain.Designer.cs
+++ TransformerBank/frmMain.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace BankSystem
+﻿namespace TransformerBank
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Location = new System.Drawing.Point(100, 200);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "ATM";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
